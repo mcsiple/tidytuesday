f12594ff971f9fce9431e1a9cf1bcbbb1288bebf
Megsie Siple <mcsiple@gmail.com>
2020-03-01 19:41:09 -0800

    Cleanup excel read code

--- Other/DemCandidateScorecards.R
+++ Other/DemCandidateScorecards.R
@@ -1,29 +1,25 @@
 library(tidyverse)
 library(reshape2)
+library(readxl)
 
-setwd('~/Dropbox/Fun Side Projects/Scorecard')
-dat <- data.frame(stringsAsFactors=FALSE,
-              Candidate = c("Warren", "Sanders", "Buttegieg", "Klobuchar", "Steyer",
-                            "Bloomburg", "Biden"),
-                Sunrise = c(0.855, 0.915, NA, NA, NA, NA, 0.375),
-          UrbanRacialEq = c(0.91, 0.88, 0.86, 0.735, 0.675, 0.5525, 0.615),
-            Indivisible = c(0.95, 0.89, 0.77, 0.57, 0.75, NA, 0.5),
-             Climate350 = c(1, 1, 0.5, 0.5, 1, 0.25, 0.25),
-          CenterBiolDiv = c(0.8, 1, 0.4, 0.1, NA, 0.1, 0.2),
-           Endorsement.Index.538 = c(95, 68, 50, 60, 2, 108, 287)/287,
-                   Root = c(79, 50, 66, 22, 62, 43, 70)/79
-       )
+dat <- read_excel(here::here('Other','ScorecardData.xlsx'),sheet = 2) %>%
+  as.data.frame()
+dat[,-1] <- as.numeric(unlist(dat[,-1]))
 
-dt <- melt(dat)
+# Sloppy scaling
+dat$Endorsements <- dat$Endorsements/max(dat$Endorsements)
+dat$Root <- dat$Root/max(dat$Root)
 
+dt <- pivot_longer(data = dat,-Candidate) %>%
+        mutate(name = recode(name, 'Endorsements'='Endorsement.Index.538'))
 
 png('Scorecard.png',width = 8,height = 6,units = 'in',res = 150)
 dt %>% filter(Candidate != "Steyer") %>%
-  ggplot(aes(x=variable,y=value,colour=-value)) +
+  ggplot(aes(x=name,y=value,colour=value)) +
   geom_point(size=2.5) +
   xlab('Scorecard') +
   ylab('Score') +
-  scale_colour_viridis_c() +
+  scale_colour_viridis_c(direction = -1) +
   facet_wrap(~Candidate,scales="free_y") +
   theme_classic(base_size=16) +
   theme(axis.text.x = element_text(angle=45,hjust = 1),
