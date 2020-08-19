f25cc82fb92cbfb09d1ce28a0a4e5a78a3cd4938
Megsie Siple <mcsiple@gmail.com>
2020-08-18 20:58:38 -0700

    initial commit :seedling:

---
+++ 34_ExoticPlants.R
@@ -0,0 +1,88 @@
+#34_ExoticPlants
+library(tidyverse)
+library(ggchicklet)
+
+source('HelperFunctions.R')
+
+tuesdata <- tidytuesdayR::tt_load(2020, week = 34)
+
+plants <- tuesdata$plants
+actions <- tuesdata$actions
+threats <- tuesdata$threats
+
+extrafont::loadfonts()
+
+
+# Palettes ----------------------------------------------------------------
+dangerpal <- calecopal::cal_palette(name = 'superbloom3')[1:2]
+threatpal <- calecopal::cal_palette(name = 'superbloom2') %>% 
+  lengthen_pal(x = 1:12)
+bgcolor <- "#FFFFF8"
+countrycolor <- "#dfdacd"
+
+world_map <- map_data("world")
+
+plant_statuses <- plants %>% 
+  select(country, group, red_list_category) %>%
+  group_by(country,red_list_category) %>%
+  count() %>%
+  right_join(world_map,by = c("country" = "region") ) %>%
+  filter(!is.na(red_list_category))
+
+
+p1 <- ggplot() +
+  geom_polygon(data = world_map, aes(x = long, y = lat, group = group),
+               fill = countrycolor, colour = "grey45", size = 0.1) +
+  geom_polygon(data = plant_statuses,
+               aes(x = long, y = lat, group = group,fill = log(n)), 
+               colour = "grey45", size = 0.1) +
+  facet_wrap(~red_list_category, ncol = 2) +
+  theme_void(base_family = 'Lato') +
+  scale_fill_stepsn('Number of \nspecies \n(log-scale)',colours = dangerpal) +
+  theme(panel.background = element_rect(fill = bgcolor,colour = bgcolor),
+        plot.background = element_rect(fill = bgcolor,colour = bgcolor),
+        panel.grid = element_blank(),
+        legend.position = 'right')
+p1
+
+
+# Chicklet plots ----------------------------------------------------------
+d2 <- threats %>%
+  group_by(red_list_category,continent,threat_type) %>%
+  summarize(nthreatened = sum(threatened,na.rm = TRUE))
+
+p2 <- d2 %>% 
+  ggplot(aes(continent, nthreatened,fill = threat_type)) +
+  geom_chicklet(width = 0.75,colour = bgcolor) +
+  scale_fill_manual("Threat type",values = threatpal) +
+  xlab("Continent") +
+  ylab("Number of species") +
+  theme(text = element_text(family = 'Lato'),
+        panel.background = element_rect(fill = bgcolor,colour = bgcolor),
+        plot.background = element_rect(fill = bgcolor,colour = bgcolor),
+        legend.background = element_rect(fill = bgcolor, colour = bgcolor),
+        legend.key = element_rect(fill = bgcolor, color = NA),
+        panel.grid = element_blank(),
+        strip.background = element_blank(),
+        legend.position = 'bottom') +
+  facet_wrap(~red_list_category,ncol = 2,scales = "free_x") +
+  coord_flip()
+p2
+
+
+library(patchwork)
+
+all <- p1 + p2 + 
+  plot_layout(heights = c(3,1)) + 
+  plot_annotation(title = 'Plants in danger',
+                  subtitle = 'Threats to plants that are extinct or extinct in the wild',
+                  caption = 'Data: Florent Laverne & Cédric Scherer') & 
+  theme(text = element_text('Lato',size = 14),
+        panel.background = element_rect(fill = bgcolor,colour = bgcolor),
+        plot.background = element_rect(fill = bgcolor,colour = bgcolor))
+
+png('Plants.png',width = 12,height = 9,units = 'in',res = 200)
+all
+dev.off()
+
+#ggsave(all,filename = 'Plantsgg.png',width = 14,height = 10,units = 'in')
