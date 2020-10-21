8be5a0fe0e1571ab32e7e93a5a1f9e78962a19dc
Megsie Siple <mcsiple@gmail.com>
2020-10-20 19:34:30 -0700

    update to helper notes

--- HelperFunctions.R
+++ HelperFunctions.R
@@ -14,6 +14,21 @@ lengthen_pal <- function(x=1:10,shortpal){
 }
 
 
-# Note! -------------------------------------------------------------------
+# PATCHWORK NOTE -------------------------------------------------------------------
 #if you use patchwork, use the & symbol to add a theme to the whole assemblage
 # p1 + p2 + plot_annotation(title = 'Patchwork plots',subtitle = 'Reminder about adding a theme to the whole shebang') & theme(text = element_text('Roboto Condensed',size = 16))
+
+
+# MAP NOTES ---------------------------------------------------------------
+# world_map <- map_data("world")
+# ggplot(world_map) +
+#   geom_polygon(data = world_map, aes(x = long, y = lat, group = group), 
+#                fill = "grey30", colour = "grey45", size = 0.1) 
+
+
+# Add how to print palette in graph window --------------------------------
+#taken from stackoverflow
+show_pal <- function(pal){ 
+  plot(seq_len(length(pal)), rep_len(1, length(pal)),
+     col = pal, pch = 16, cex = 3, xaxt = 'n', yaxt = 'n', xlab = '', ylab = '')
+}
