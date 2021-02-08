52c5c5420d79bea4ee339c7644f805be6b0ddf4f
Megsie Siple <mcsiple@gmail.com>
2021-02-07 16:29:35 -0800

    summarize - top four companies + "others" found at cleanup

--- 5_2021_plastics.R
+++ 5_2021_plastics.R
@@ -4,13 +4,39 @@ library(tidyverse)
 tuesdata <- tidytuesdayR::tt_load(2021, week = 5)
 plastics <- tuesdata$plastics
 totplastics <- plastics %>% 
-  group_by(country, year) %>%
+  group_by(parent_company, year) %>%
   summarize_at(vars(empty:pvc), .funs = ~ sum(.x, na.rm = TRUE))
 
-totplastics %>% 
+
+top3 <- totplastics %>% 
   filter(year == 2020) %>%
   pivot_longer(cols = empty:pvc, names_to = "type") %>%
-  ggplot(aes(x = country, y = value, fill = type, colour = type)) +
+  group_by(type) %>%
+  summarize(total= sum(value, na.rm=T)) %>%
+  ungroup() %>%
+  slice_max(total, n = 3) 
+
+top4  <- totplastics %>% 
+  filter(year == 2020) %>%
+  pivot_longer(cols = empty:pvc, names_to = "type") %>%
+  group_by(parent_company) %>%
+  summarize(total = sum(value, na.rm=T)) %>%
+  ungroup() %>%
+  mutate(parent_company = case_when(
+    parent_company %in% c("Unbranded","null","NULL") ~ "Unbranded_unknown", 
+    TRUE ~ parent_company)) %>%
+  mutate(company_lumped = fct_lump(parent_company, n = 4, w = total)) %>%
+  distinct(parent_company, company_lumped) %>%
+  right_join(totplastics) %>%
+  mutate(total = hdpe + ldpe + o +  pet + pp  +  ps + pvc) # forgot how to sum across columns
+
+
+top4 %>%
+  pivot_longer(cols = empty:pvc, names_to = "type") %>%
+  mutate(type = fct_reorder(type, desc(value))) %>%
+  filter(company_lumped!= "Unbranded_unknown") %>%
+  ggplot(aes(x = type, y = value, fill = type, colour = type)) +
   geom_col() +
-  coord_polar(theta = "y")
+  #coord_polar(theta = "y") +
+  facet_wrap(~company_lumped)
   
